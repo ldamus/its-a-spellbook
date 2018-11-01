807ff24acf69858528edeb350a7a1fdb46d9b898
ldamus <32778408+ldamus@users.noreply.github.com>
2018-11-01 09:37:41 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,10 @@
+$(".Accio").click(function() {
+    $(".object").delay(5000).toggle(900);
+    
+});
+$(".Avada-Kedavra").click(function(){
+    $(".death").fadeToggle();
+});
+$(".Crucio").click(function() {
+    $(".torture").slideToggle();
+});
